be89d2f1cca4ab9333178d617a52edace885b034
unknown <nordin.haouari@gmail.com>
2014-09-19 16:25:19 +0200

    Refactoring based on metrics-jersey implementation

--- src/main/java/com/github/xavierbourguignon/metrics/resteasy/MetricsFeature.java
+++ src/main/java/com/github/xavierbourguignon/metrics/resteasy/MetricsFeature.java
@@ -1,99 +1,144 @@
 package com.github.xavierbourguignon.metrics.resteasy;
 
+import static com.codahale.metrics.MetricRegistry.name;
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+
+import org.jboss.resteasy.core.interception.PostMatchContainerRequestContext;
+
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import com.codahale.metrics.annotation.Metered;
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.base.Joiner;
 
-import javax.ws.rs.*;
-import javax.ws.rs.container.DynamicFeature;
-import javax.ws.rs.container.ResourceInfo;
-import javax.ws.rs.core.FeatureContext;
-import javax.ws.rs.ext.Provider;
-import java.lang.reflect.Method;
-
-import static com.codahale.metrics.MetricRegistry.name;
-
-@Provider
-@ConstrainedTo(RuntimeType.SERVER)
 public class MetricsFeature implements DynamicFeature {
 
-    private final MetricRegistry registry;
+	private final MetricRegistry registry;
+	private Map<Method, Timer> timers = newHashMap();
+	private Map<Method, Meter> meters = newHashMap();
+	private TimerRequestEventListener timerRequestEventListener;
+	private MeterRequestEventListener meterRequestEventListener;
 
-    public MetricsFeature(MetricRegistry registry) {
-        this.registry = registry;
-    }
+	public MetricsFeature(MetricRegistry registry) {
+		this.registry = registry;
+		timerRequestEventListener = new TimerRequestEventListener(timers);
+		meterRequestEventListener = new MeterRequestEventListener(meters);
+	}
 
-    @Override
-    public void configure(ResourceInfo resourceInfo, FeatureContext context) {
-        Method resourceMethod = resourceInfo.getResourceMethod();
-        if (resourceMethod.isAnnotationPresent(Timed.class)) {
-            final Timed annotation = resourceMethod.getAnnotation(Timed.class);
-            final String name = chooseName(annotation.name(), annotation.absolute(), resourceInfo);
-            final Timer timer = registry.timer(name);
-            context.register(new TimedInterceptor(timer));
-        }
+	private static class TimerRequestEventListener implements ContainerRequestFilter, ContainerResponseFilter {
+		private final Map<Method, Timer> timers;
+		private Timer.Context context = null;
 
-        if (resourceMethod.isAnnotationPresent(Metered.class)) {
-            final Metered annotation = resourceMethod.getAnnotation(Metered.class);
-            final String name = chooseName(annotation.name(), annotation.absolute(), resourceInfo);
-            final Meter meter = registry.meter(name);
-            context.register(new MeterInterceptor(meter));
-        }
-    }
+		public TimerRequestEventListener(final Map<Method, Timer> timers) {
+			this.timers = timers;
+		}
 
-    private String chooseName(String explicitName, boolean absolute, ResourceInfo resourceInfo) {
-        if (explicitName != null && !explicitName.isEmpty()) {
-            if (absolute) {
-                return explicitName;
-            }
-            return name(getName(resourceInfo), explicitName);
-        }
+		public void filter(ContainerRequestContext requestContext) throws IOException {
+			PostMatchContainerRequestContext postMatchContainerRequestContext = (PostMatchContainerRequestContext) requestContext;
+			final Timer timer = this.timers.get(postMatchContainerRequestContext.getResourceMethod().getMethod());
+			if (timer != null) {
+				this.context = timer.time();
+			}
+		}
 
-        return getName(resourceInfo);
-    }
+		public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+				throws IOException {
+			if (this.context != null) {
+				this.context.close();
+			}
+		}
+	}
 
-    private String getName(ResourceInfo resourceInfo) {
-        return getMethod(resourceInfo.getResourceMethod()) + " - " + getPath(resourceInfo);
-    }
+	private static class MeterRequestEventListener implements ContainerRequestFilter {
+		private final Map<Method, Meter> meters;
 
-    private String getPath(ResourceInfo resourceInfo) {
-        String rootPath = null;
-        String methodPath = null;
+		public MeterRequestEventListener(final Map<Method, Meter> meters) {
+			this.meters = meters;
+		}
 
-        if (resourceInfo.getResourceClass().isAnnotationPresent(Path.class)) {
-            rootPath = resourceInfo.getResourceClass().getAnnotation(Path.class).value();
-        }
+		public void filter(ContainerRequestContext requestContext) throws IOException {
+			PostMatchContainerRequestContext postMatchContainerRequestContext = (PostMatchContainerRequestContext) requestContext;
+			final Meter meter = this.meters.get(postMatchContainerRequestContext.getResourceMethod().getMethod());
+			if (meter != null) {
+				meter.mark();
+			}
+		}
+	}
 
-        if (resourceInfo.getResourceMethod().isAnnotationPresent(Path.class)) {
-            methodPath = resourceInfo.getResourceMethod().getAnnotation(Path.class).value();
-        }
+	public void configure(ResourceInfo resourceInfo, FeatureContext context) {
+		registerTimedAnnotations(getResourceMethod(resourceInfo));
+		registerMeteredAnnotations(getResourceMethod(resourceInfo));
 
-        return Joiner.on("/").skipNulls().join(rootPath, methodPath);
-    }
+		context.register(timerRequestEventListener);
+		context.register(meterRequestEventListener);
+	}
 
-    private String getMethod(Method resourceMethod) {
-        if (resourceMethod.isAnnotationPresent(GET.class)) {
-            return HttpMethod.GET;
-        }
-        if (resourceMethod.isAnnotationPresent(POST.class)) {
-            return HttpMethod.POST;
-        }
-        if (resourceMethod.isAnnotationPresent(PUT.class)) {
-            return HttpMethod.PUT;
-        }
-        if (resourceMethod.isAnnotationPresent(DELETE.class)) {
-            return HttpMethod.DELETE;
-        }
-        if (resourceMethod.isAnnotationPresent(HEAD.class)) {
-            return HttpMethod.HEAD;
-        }
-        if (resourceMethod.isAnnotationPresent(OPTIONS.class)) {
-            return HttpMethod.OPTIONS;
-        }
+	private void registerTimedAnnotations(final Method method) {
+		final Timed annotation = method.getAnnotation(Timed.class);
 
-        throw new IllegalStateException("Resource method without GET, POST, PUT, DELETE, HEAD or OPTIONS annotation");
-    }
+		if (annotation != null) {
+			timers.put(method, timerMetric(registry, method, annotation));
+		}
+	}
+
+	private void registerMeteredAnnotations(final Method method) {
+		final Metered annotation = method.getAnnotation(Metered.class);
+
+		if (annotation != null) {
+			meters.put(method, meterMetric(registry, method, annotation));
+		}
+	}
+
+	private static Timer timerMetric(final MetricRegistry registry,
+			final Method method,
+			final Timed timed) {
+		final String name = chooseName(timed.name(), timed.absolute(), method);
+		return registry.timer(name);
+	}
+
+	private static Meter meterMetric(final MetricRegistry registry,
+			final Method method,
+			final Metered metered) {
+		final String name = chooseName(metered.name(), metered.absolute(), method);
+		return registry.meter(name);
+	}
+
+	protected static String chooseName(final String explicitName, final boolean absolute, final Method method,
+			final String... suffixes) {
+		if (explicitName != null && !explicitName.isEmpty()) {
+			if (absolute) {
+				return explicitName;
+			}
+			return name(method.getDeclaringClass(), explicitName);
+		}
+
+		return name(name(method.getDeclaringClass(),
+				method.getName()),
+				suffixes);
+	}
+
+	private Method getResourceMethod(ResourceInfo resourceInfo) {
+		try {
+			String methodName = resourceInfo.getResourceMethod().getName();
+			Class<?>[] methodParameterTypes = resourceInfo.getResourceMethod().getParameterTypes();
+			return resourceInfo
+					.getResourceClass()
+					.getMethod(methodName, methodParameterTypes);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
